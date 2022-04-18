2e10a9984a95298016aa9a295ec153dac02724e9
Ike Eveland <88570218+Soukoku2383@users.noreply.github.com>
2022-04-18 12:28:20 +0800

    Add files via upload

--- db_createwindow.cpp
+++ db_createwindow.cpp
@@ -1,6 +1,13 @@
 #include "db_createwindow.h"
 #include "ui_db_createwindow.h"
-
+#include <QMessageBox>
+#include <string.h>
+#include <io.h>
+#include <QFile>
+#include <QTextStream>
+#include <QFileDialog>
+#include <QDateTime>
+using namespace std;
 DB_CreateWindow::DB_CreateWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DB_CreateWindow)
@@ -15,7 +22,63 @@ DB_CreateWindow::~DB_CreateWindow()
 
 void DB_CreateWindow::on_confirmButton_clicked()
 {
+    QString s = ui->textEdit->toPlainText();//读取用户输入的数据库名称
+    int length =  s.length();
+    if(length>128)
+    {
+        QMessageBox::question(this,
+        tr("error"),
+        tr("数据库名称不得超过128个字符"),
+        QMessageBox::Ok | QMessageBox::Cancel,
+        QMessageBox::Ok);
+    }
+    //长度符合之后
+    string dataBaseName = s.toStdString();
+    string pathName = "D:\\simpleDBMS\\" + dataBaseName;//文件夹位置，相当于数据库
+    string fileName = pathName + "\\db.txt";//文件位置，存放数据库的相关信息
+    QString qs = QString::fromStdString(fileName);
+    QFile file(qs);
+    if (0 != access(pathName.c_str(), 0))
+    {
+       if (0 == mkdir(pathName.c_str()))//返回0表示创建成功,-1表示失败
+       {
+           if(! file.open(QIODevice::Append|QIODevice::Text))  //append追加，不会覆盖之前的文件
+           {
+                   QMessageBox::critical(this,"错误","文件打开失败，信息没有保存！","确定");
+                   return;
+           }
+          QMessageBox::question(this,
+          tr("success"),
+          tr("创建成功"),
+          QMessageBox::Ok | QMessageBox::Cancel,
+          QMessageBox::Ok);
+        }
+          else
+       {
 
+          QMessageBox::question(this,
+          tr("error"),
+          tr("创建失败"),
+          QMessageBox::Ok | QMessageBox::Cancel,
+          QMessageBox::Ok);
+        }
+
+     }
+    else
+    {
+          QMessageBox::question(this,
+          tr("error"),
+          tr("该数据库已存在"),
+          QMessageBox::Ok | QMessageBox::Cancel,
+          QMessageBox::Ok);
+    }
+
+          QFileInfo info(qs);
+          QString str = info.birthTime().toString();
+          QString dbInfo = s+"#"+ QString::fromStdString(pathName)+"#"+str+"#"+"用户数据库";//#作为分隔符
+          QTextStream out(&file);//写入
+          out << dbInfo;
+          file.close();
     this->close();
 }
 
--- db_deletewindow.cpp
+++ db_deletewindow.cpp
@@ -1,6 +1,10 @@
 #include "db_deletewindow.h"
 #include "ui_db_deletewindow.h"
-
+#include <QFile>
+#include <QDir>
+#include <QString>
+#include <QMessageBox>
+#include <QLineEdit>
 DB_DeleteWindow::DB_DeleteWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::DB_DeleteWindow)
@@ -15,6 +19,25 @@ DB_DeleteWindow::~DB_DeleteWindow()
 
 void DB_DeleteWindow::on_confirmButton_clicked()
 {
+
+    QString dataBaseName = ui->lineEdit->text();  //读取用户输入的数据库名称
+    QString strPath = "D:\\simpleDBMS\\" + dataBaseName;
+    if (strPath.isEmpty() || !QDir().exists(strPath))//是否传入了空的路径||路径是否存在
+    {
+        QMessageBox::question(this,
+        tr("error"),
+        tr("找不到该数据库"),
+        QMessageBox::Ok | QMessageBox::Cancel,
+        QMessageBox::Ok);
+    }
+    QDir qDir(strPath);
+    qDir.removeRecursively();
+    QMessageBox::question(this,
+    tr("success"),
+    tr("删除成功"),
+    QMessageBox::Ok | QMessageBox::Cancel,
+    QMessageBox::Ok);
+
 
     this->close();
 }
